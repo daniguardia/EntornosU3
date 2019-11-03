a231c801bed25209e74e8493a8979db92d5cc82e
Daniel Guardia <daniguardiamail@gmail.com>
2019-11-03 17:17:54 +0100

    Modificada la rama cambio

--- src/entornosu3/EntornosU3.java
+++ src/entornosu3/EntornosU3.java
@@ -16,7 +16,7 @@ public class EntornosU3 {
     if (valor1.equals(valor2)) {
         return "Bien. Pero una hora de estudio para cada módulo puede ser insuficiente.";
     }
-    if (Integer.valueOf(valor1) < Integer.valueOf(valor2)){
+    if (Integer.valueOf(valor1) > Integer.valueOf(valor2)){
         return "Poco tiempo de estudio. Debes dedicar más tiempo.";
     }
     return "Ideal. Trabajas los contenidos en casa.";
